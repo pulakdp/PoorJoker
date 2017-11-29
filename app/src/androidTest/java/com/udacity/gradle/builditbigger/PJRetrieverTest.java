8751782b93c455218f0f72f1bed5d101f0cd2018
pulak03 <pulakdebasish@gmail.com>
2017-11-29 13:29:12 +0530

    Finished

--- app/src/androidTest/java/com/udacity/gradle/builditbigger/PJRetrieverTest.java
+++ app/src/androidTest/java/com/udacity/gradle/builditbigger/PJRetrieverTest.java
@@ -9,8 +9,8 @@ public class PJRetrieverTest extends AndroidTestCase{
 
     public void testReturnsNonEmptyString() {
         String result = null;
-        PJRetriever pjRetriever = new PJRetriever();
-        pjRetriever.execute(getContext());
+        PJRetriever pjRetriever = new PJRetriever(getContext(), null);
+        pjRetriever.execute();
         try {
             result = pjRetriever.get();
         }catch (Exception e) {
